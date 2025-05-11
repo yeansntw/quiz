a77001769f472fc6cadf5414c300b0526d79b181
yeans.ntw <nanthawut.tsw@gmail.com>
2025-05-11 23:18:33 +0700

    update knowledge

--- app/data/knowledge.tsx
+++ app/data/knowledge.tsx
@@ -149,6 +149,231 @@ const quizData = [
         ],
         correctAnswer: 0,
     },
+    {
+        question: "ในเด็กที่พบว่ามี brain turmor มักพบอาการอะไรร่วมด้วยคือ",
+        choices: [
+            "Hydrocephalus",
+            "Hypertrophy",
+            "Hypotonicity",
+            "Hypertonicity",
+        ],
+        correctAnswer: 0,
+    },
+    {
+        question: "จุดกดเจ็บของกล้ามเนื้อใดที่มีอาการร้าวไปบริเวณกลางศีรษะ ท้ายทอย บริเวณในตา หน้าผาก และกกหู",
+        choices: [
+            "Suboccioital muscle",
+            "Sternocleidomastoid muscle",
+            "Spenius capitis muscle",
+            "Levator scapulae muscle",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "ใน 1 นาที มีอากาศไหลเข้าสู่ปอดประมาณกี่ลิตรในคนปกติขณะพัก",
+        choices: [
+            "1L/min",
+            "2L/min",
+            "15L/min",
+            "6L/min",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "การวินิฉัยโรค MS ในทางคลินิกจะต้องพบว่ามีพยาธิสภาพเกิดขึ้นที่ใด และอย่างน้อยกี่ตำแหน่ง",
+        choices: [
+            "Gray matter - 1 ตำแหน่ง",
+            "White matter - 1 ตำแหน่ง",
+            "Gray matter - 2 ตำแหน่ง",
+            "White matter - 2 ตำแหน่ง",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "Mallet finger เกิดขึ้นที่ข้อต่อใด",
+        choices: [
+            "DIP",
+            "PIP",
+            "MCP",
+            "DIP&PIP",
+        ],
+        correctAnswer: 0,
+    },
+    {
+        question: "ผู้ป่วยได้รับบาดเจ็บของไขสันหลังระดับ C6 ควบคุมการขับถ่ายไม่ได้ และสูญเสียความรู้สึกทุกชนิด ยกเว้น proprioceptive, vibration และ tactile senses และไม่สามารถควบคุมการทำงานของกล้ามเนื้อทั้งสองซีกของร่างกายในส่วนที่ต่ำกว่ารอยโรค สรุปการวินิฉัยอย่างไร",
+        choices: [
+            "Incomplete quadriplegia C6 (gr.B)",
+            "Incomplete quadriplegia C5 (gr.B)",
+            "Incomplete quadriplegia C5 (gr.C)",
+            "Incomplete quadriplegia C6 (gr.C)",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "ข้อใดเป็น red fleg ที่บ่งบอกถึงพัฒนาการล่าช้าในเด็กอายุ 1 ปี 2 เดือน",
+        choices: [
+            "ไม่เล่นกับเพื่อน",
+            "ไม่บอกขับถ่าย",
+            "พูดเป็นคำไม่มีความหมาย",
+            "สื่อสารภาษากายไม่ได้",
+            "เดินไม่ได้",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "เด็กในข้อใดที่มีความเสี่ยงในพัฒนาการการเคลื่อนไหวล่าช้า",
+        choices: [
+            "6 เดือน ไม่สามารถนั่งทรงตัวโดยใช้มือยันเพื่อน",
+            "3 เดือน ไม่สามารถพลิกคว่ำได้ และไม่สามารถเล่นของแนวกลางลำตัวได้",
+            "4 เดือน ไม่สามารถลุกขึ้นมานั่งจากท่านอนหงาย",
+            "10 เดือน ไม่สามารถยืนและเดินได้",
+            "8 เดือน ไม่สามารถคืบและกลิ้ง",
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: "รูปแบบการเดินของผู้ป่วย Pakinson คือข้อใด",
+        choices: [
+            "ataxic gait",
+            "antalgic gait",
+            "shuffle gait",
+            "circumduction gait",
+            "E Trendelenbrug gait",
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: "ข้อใดเป็นการรักษา chroinc Peripheral artery disease",
+        choices: [
+            "ลดบวมโดยถาวร elevation & pumping exercise",
+            "เพิ่ม walking capacity by exercise training",
+            "เพิ่ม blood circulation โดย superficial heat",
+            "ลด claudication by message",
+            "Decrease exercise pain ให้ rest",
+        ],
+        correctAnswer: 1,
+    },
+     {
+        question: "เทคนิคกระตุ้น Brachial plexus injury แบบ complete denervated",
+        choices: [
+            "Monopolar, supramax intensity เห็นการหดตัวของกล้ามเนื้อ",
+            "Bipolar, sub max intensity เห็นการหดตัวของกล้ามเนื้อ",
+            "Bipolar, supramax intensity เห็นการหดตัวของกล้ามเนื้อ",
+            "Bipolar minimum intensity เห็นการหดตัวของกล้ามเนื้อ",
+            "Monopolar, max intensity comfortable เห็นการหดตัวของกล้ามเนื้อ",
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: "โรคที่พบโปรตีนในปัสสาวะคือโรคใด",
+        choices: [
+            "Myotonic Dystrophy (MD)",
+            "Guillain-Barre Syndrome (GBS)",
+            "Amyotrophic Lateral Sclerosis (ALS)",
+            "Myasthenia Gravis (MG)",
+            "Multiple Sclerosis (MS)",
+        ],
+        correctAnswer: 0,
+    },
+     {
+        question: "ข้อใดไม่เหมาะสมในการป้องกันภาวะแทรกซ้อนระบบหายใจในผู้ป่วย myasthenia gravis",
+        choices: [
+            "การฝึกหายใจเพื่อเพิ่มการระบายอากาศ",
+            "การฝึกหายใจแบบห่อปาก",
+            "การฝึกเพิ่มความแข็งแรงกล้ามเนื้อหายใจจนรู้สึกล้า",
+            "การฝึกไออย่างมีประสิทธิภาพ",
+            "การใช้เครื่องช่วยหายใจเมื่อมีอาการเหนื่อย",
+        ],
+        correctAnswer: 2,
+    },
+     {
+        question: "ข้อใดไม่ถูกต้องในการรักษาเพื่อลดบวมในผู้ป่วยที่มีความผิดปกติของระบบน้ำเหลือง",
+        choices: [
+            "การออกกำลังกาย แบบ Isotonic pumping exercise",
+            "นวดแบบ stationary circles",
+            "นวดแบบ scooping ร่วมกับจัดท่าทาง",
+            "การออกกำลังกาย แบบ Passive pumping exercise",
+            "การใช้เครื่อง intermittent mechanical compression",
+        ],
+        correctAnswer: 3,
+    },
+     {
+        question: "ข้อใดคือการรักษา ป้องกันที่เหมาะสมสำหรับการหดรั้งที่ ANTERIOR AXILLARY FOLD",
+        choices: [
+            "shoulder flexion",
+            "shoulder adbuction",
+            "shoulder extension",
+            "shoulder internal rotation",
+            "shoulder external rotation",
+        ],
+        correctAnswer: 2,
+    },
+    {
+        question: "ข้อใดเป็นปัญหาที่พบน้อยในผู้ป่วยที่มีภาวะ adolescent scoliosis",
+        choices: [
+            "imbalance of muscle",
+            "Poor breathing",
+            "Postural imbalance",
+            "Poor posture",
+            "Nerve radicular pain",
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: "ข้อใดไม่ใช่การรักษาในผู้ป่วยโรคข้อเสื่อม",
+        choices: [
+            "เพิ่มความแข็งแรงของกล้ามเนื้อรอบข้อเข่า",
+            "ลดการผิดรูปของข้อเข่า",
+            "ลดปวดของข้อเข่า",
+            "เพิ่มความทนทานของกล้ามเนื้อรอบข้อเข่า",
+            "เพิ่มความยืดหยุ่นของกล้ามเนื้อรอบข้อเข่า",
+        ],
+        correctAnswer: 1,
+    },
+    {
+        question: "ปัญหาในข้อใดพบได้น้อยในผู้ป่วย Adolescent idiopathic scoliosis",
+        choices: [
+            "Postural imbalance",
+            "Breathing problem",
+            "Muscle imbalance",
+            "Nerve radiated pain",
+            "Poor posture",
+        ],
+        correctAnswer: 3,
+    },
+     {
+        question: "ผู้สูงอายุมีความบกพร่องใดจะพบการเดินแบบ antalgic gait",
+        choices: [
+            "tightness of hip adductor",
+            "weakness of quadriceps",
+            "impaired knee proprioception",
+            "hip join pain",
+            "tightness of hamstring",
+        ],
+        correctAnswer: 3,
+    },
+    {
+        question: "การฝึกใดในนักกีฬาบาสที่เหมาะกับทักษะการหลบหลีกคู่ต่อสู้",
+        choices: [
+            "coordination",
+            "speed",
+            "power",
+            "balance",
+            "Agility",
+        ],
+        correctAnswer: 4,
+    },
+    {
+        question: "ข้อใดไม่ถูกต้องเกี่ยวกับการออกกำลังกายหลังผ่าตัด mastectomy",
+        choices: [
+            "pendulum exercise หลังผ่าตัดทันที",
+            "manual resistance exercises หลังผ่าตัด 1 สัปดาห์",
+            "AROM exercises หลังผ่าตัด 1 สัปดาห์",
+            "ลดบวมด้วย pumping exercises",
+            "active exercise ขยับข้อศอกและข้อมือให้เร็วที่สุด",
+        ],
+        correctAnswer: 0,
+    },
 ]
 
 export default quizData;
